acf5fb2db47aff4815e7fc7c6a47ba2482d5fbd7
devarthurguilherme <arthurguilherme2013@gmail.com>
2023-11-10 06:01:16 +0000

    Lessons: Image Cards and Font Optimization

--- First-Project/app/reviews/layout.jsx
+++ First-Project/app/reviews/layout.jsx
@@ -1,9 +1,6 @@
 export default function ReviewsLayout({ children }) {
     return (
         <div>
-            <div>
-                [reviews menubar]
-            </div>
             {children}
         </div>
     )
--- First-Project/app/reviews/page.jsx
+++ First-Project/app/reviews/page.jsx
@@ -3,19 +3,43 @@ import Link from "next/link";
 import Heading from "@/components/Heading";
 
 const Links = [
-    {id: 1, path: "/reviews/hollow-knight", description: "Hollow Knight", image: "/images/stardew-valley.jpg"}
+    {
+        id: 1, 
+        path: "/reviews/hollow-knight", 
+        description: "Hollow Knight", 
+        image: "/images/hollow-knight.jpg",
+    },
+    {
+        id: 2, 
+        path: "/reviews/stardew-valley", 
+        description: "Stardew Valley", 
+        image: "/images/stardew-valley.jpg"
+    }
 ]
 
 export default function ReviewsPage() {
     return (
         <>
             <Heading>Reviews</Heading>
-            <ul>
+            <ul className="flex flex-col gap-3">
                 {Links.map((item) => {
                     return(
-                        <li key={item.id}>
+                        <li
+                            className="bg-white border rounded shadow w-80 hover:shadow-xl" 
+                            key={item.id}
+                        >
                             <Link href={item.path}>
-                                <img src={item.image} alt="" /> {item.description}
+                                <img 
+                                    src={item.image} 
+                                    alt={item.description}
+                                    width="320"
+                                    height="180"
+                                    className="mb-2 rounded-t"
+                                    
+                                />
+                                <h2 className="py-1 text-center">
+                                    {item.description}
+                                </h2>
                             </Link>
                         </li>
                     )
